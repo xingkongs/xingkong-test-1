1a82e9148e8c8ecfca594cc012a6969b716591ee
xingkongs <ldk8259878@163.com>
2018-07-19 17:19:53 +0800

    完成 button and input

---
+++ src/icon.vue
@@ -0,0 +1,22 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+
+<script>
+    import './svg'
+    export default {
+        name: "icon",
+        props:{
+            name
+        }
+    }
+</script>
+
+<style scoped>
+.g-icon{
+    width:1em;
+    height:1em;
+}
+</style>
